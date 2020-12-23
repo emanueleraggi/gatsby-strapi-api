265af54bf2b942731c60438b6efa40504a854d46
blueviewLaptop <croman2@uri.edu>
2020-12-23 16:14:02 +0000

    api for the gatsby strapi portfolio landing page

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('dlfvkm51z'),
+      api_key: env('142628118258853'),
+      api_secret: env('KohlQKSz1B9LvKcxri7Dh6S4dAk'),
+    },
+  },
+  // ...
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec93ee72-699e-4e1a-8702-9a381d7244e3'
+};
